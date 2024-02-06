fa3d14c0bd4c9ceb1a1fde368b2dff45f7f95186
Vishal11-sajjan <60094016+Vishal11-sajjan@users.noreply.github.com>
2024-02-06 23:33:18 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongouri = "mongodb+srv://ujjawal:ujjawal123@crud-app.bm61lxf.mongodb.net/getVisit?retryWrites=true&w=majority" //see here database name
+const mongouri = "mongodb+srv://vishalsajjan:World2024@cluster0.jdvdzvb.mongodb.net/iit?retryWrites=true&w=majority" //see here database name
 const mongoConnect = mongoose.connect(mongouri, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('Connected to the database'); 
